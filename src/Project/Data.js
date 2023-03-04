a2f2b0b69e406b0aedd0e91c22982e2f8a969b84
fares <fares12yaseer45@gmail.com>
2023-03-05 00:29:34 +0200

    add the data file

---
+++ src/Project/Data.js
@@ -0,0 +1,37 @@
+    export const data = [
+        {
+            day: "mon",
+            amount: 17.45,
+            Minimum : 20
+        },
+        {
+            day: "tue",
+            amount: 34.91,
+            Minimum : 20
+        },
+        {
+            day: "wed",
+            amount: 52.36,
+            Minimum : 20
+        },
+        {
+            day: "thu",
+            amount: 31.07,
+            Minimum : 20
+        },
+        {
+            day: "fri",
+            amount: 23.39,
+            Minimum : 20
+        },
+        {
+            day: "sat",
+            amount: 43.28,
+            Minimum : 20
+        },
+        {
+            day: "sun",
+            amount: 25.48,
+            Minimum : 20
+        }
+    ]
