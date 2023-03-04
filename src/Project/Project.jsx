73e762abeefd8cc13a810a402a0df91930edec56
fares <fares12yaseer45@gmail.com>
2023-03-05 00:24:41 +0200

    create the 70% of project

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import './Style.scss'
+import Logo from './images/logo.svg'
 
 const Project = () => {
     return (
-        <div>Project</div>
+        <div className='container'>
+            <div className="balance">
+                <div className="balance-container">
+                    <div className="price">
+                        <p>My Balance</p>
+                        <h2>$200.99</h2>
+                    </div>
+                    <div className="logo">
+                        <img src={Logo} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="expenses">
+                <div className="expenses-container">
+                    <div className="title">
+                        <h2>Spending - Last 7 Days</h2>
+                    </div>
+                    <div className="charts">
+                        <div className="charts-container">
+                            <div className="chart"></div>
+                                <span>Mun</span>
+                        </div>
+                    </div>
+                    <div className="Total-Price"></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
